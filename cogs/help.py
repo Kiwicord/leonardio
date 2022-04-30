7d67fcea925fb462e46f5cced05cba3f7ba81fbe
Laurens <laurens.andre.petzold@gmail.com>
2022-04-30 09:49:08 +0200

    Report hinzugefügt

--- cogs/help.py
+++ cogs/help.py
@@ -7,7 +7,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        await ctx.reply('**Commands:**\n*Prefix:";"*\n  -help (show this list)\n  -gay (secret message)\n  -jasonisttoll (😳😳😳😳😳😳😳)\n  -clear [amount] (clear chat)\n  -8ball [question] (8ball command)\n  -ban [member] (ban command)\n  -kick [member] (kick command)\n  -load [command] (load command)\n  -unload [command] (unload command)\n*by Laurens*')
+        await ctx.reply('**Commands:**\n*Prefix:";"*\n  -help (show this list)\n  -gay (secret message)\n  -jasonisttoll (😳😳😳😳😳😳😳)\n  -clear [amount] (clear chat)\n  -8ball [question] (8ball command)\n  -ban [member] (ban command)\n  -kick [member] (kick command)\n  -load [command] (load command)\n  -unload [command] (unload command)\n  -report [member] [reason] (report command)\n  -load [command] (load command)\n*by Laurens*')
 
 def setup(client):
     client.add_cog(Help(client))
---
+++ cogs/report.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+
+class Report(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def report(self, ctx, member:discord.Member, *, report=None):
+        report_channel = discord.utils.get(ctx.guild.channels, name = 'reports')
+        if member is None:
+            return await ctx.reply("Bitte sag wenn du Reporten möchtest.")
+        if report is None:
+            return await ctx.reply("Bitte erklär den grund für den Report.")
+        else:
+            embed = discord.Embed(title="Report", description=f"{ctx.author.mention}hat {member} Reportet", color=0xE74C3C)
+            embed.set_thumbnail(url="")
+            embed.add_field(name="Weitere Infos:", value=f'{report}')
+            embed.set_footer(text=f'Reagiere mit 😪 wenn die Situation sich gekläret hat. LOL.')
+            report_message = await report_channel.send(embed=embed)
+            await report_message.add_reaction('😪')
+            await ctx.send(f'{ctx.author.mention} Dein Report wurde zu dem Team geschickt.')
+
+            try:
+                def check(reaction, user):
+                    return user == ctx.author and str(reaction.emoji) in ['😪']
+                
+                reaction, user = await commands.wait_for('raction_add', timeout=604800, check=check)
+
+                if str(reaction.emoji) == '😪':
+                    await ctx.author.send('Ihre Report wurde geprüft und gelöscht')
+
+            except Exception as e:
+                print(e)
+                    
+
+def setup(client):
+    client.add_cog(Report(client))
