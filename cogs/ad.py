16771faa58ee62825a5b7eda91c64da6339f654c
lauchrens <104091795+lauchrens@users.noreply.github.com>
2022-07-04 14:37:45 +0200

    Update ad.py

--- cogs/ad.py
+++ cogs/ad.py
@@ -8,15 +8,22 @@ class Ad(commands.Cog):
 
     @commands.has_permissions(ban_members=True)
     @commands.command()
-    async def ad(self, ctx, amount=None):
+    async def ad(self, ctx, member:discord.Member, *, amount=None):
         amount = int(amount)
         if amount is None:
             error = discord.Embed(color=0xE74C3C, title=f'🧨✨ Bitte gib den Betrag an!')
             await ctx.send(embed=error)
             return
-        else:
+        if member is None:
+            embed = discord.Embed(color=0xE74C3C, title=f'🧨✨ DU HATATATATST {amount} 🧨 zu deinem Wallet hinzugefügt')
             await open_profile(ctx.author.id)
             await update_wallet(ctx.author.id, amount)
+            await ctx.reply(embed=embed)
+        else:
+            embed = discord.Embed(color=0xE74C3C, title=f'🧨✨ DU HATATATATST {amount} 🧨 zu {member} Wallets hinzugefügt')
+            await open_profile(member.id)
+            await update_wallet(member.id, amount)
+            await ctx.reply(embed=embed)
 
 async def setup(client):
     await client.add_cog(Ad(client))
